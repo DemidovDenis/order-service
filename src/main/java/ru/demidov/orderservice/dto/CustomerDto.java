8887168e425107d28c45dcc982a64e42768a6268
JavaDen44 <dem.denis44@gmail.com>
2024-01-27 03:39:01 +0300

    В пакете dto для классов создал геттеры и сеттеры

--- src/main/java/ru/demidov/orderservice/dto/CustomerDto.java
+++ src/main/java/ru/demidov/orderservice/dto/CustomerDto.java
@@ -1,11 +1,28 @@
 package ru.demidov.orderservice.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@Data
-@AllArgsConstructor
 public class CustomerDto {
     private Long id;
     private String username;
+
+    public CustomerDto(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
